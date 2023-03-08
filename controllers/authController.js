5a31a725c6aa01b23d3421022ac0fe9a71010546
EvanilsonP <pevanilson99@gmail.com>
2023-03-08 15:48:19 -0300

    mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
 const User = require('../models/User');
 
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+// duplicate error code
+    if(err.code === 11000) {
+        errors.email = 'that email is already registered.';
+        return errors;
+    }
+
+// validation errors
+    if(err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 };
@@ -17,8 +38,8 @@ module.exports.signup_post = async (req, res) => {
     } 
     
     catch (err) {
-        console.log(err);
-        res.status(400).send('Something went wrong.');
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 };
 
